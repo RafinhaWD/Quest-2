0281492dd3f7b3925339845da6cc38f9b15b7a20
Rafael William Dresch <117855490+RafinhaWD@users.noreply.github.com>
2024-03-29 19:02:56 -0300

    Update Palindromo.java

--- Palindromo.java
+++ Palindromo.java
@@ -1,24 +1,31 @@
-# Compiled class file
-*.class
+import java.util.Scanner;
 
-# Log file
-*.log
-
-# BlueJ files
-*.ctxt
-
-# Mobile Tools for Java (J2ME)
-.mtj.tmp/
-
-# Package Files #
-*.jar
-*.war
-*.nar
-*.ear
-*.zip
-*.tar.gz
-*.rar
-
-# virtual machine crash logs, see http://www.java.com/en/download/help/error_hotspot.xml
-hs_err_pid*
-replay_pid*
+public class Main {
+    
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        int left = 0;
+        int right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma string para verificar se é um palíndromo: ");
+        String inputString = scanner.nextLine();
+        
+        if (isPalindrome(inputString)) {
+            System.out.println("A string é um palíndromo.");
+        } else {
+            System.out.println("A string não é um palíndromo.");
+        }
+    }
+}
